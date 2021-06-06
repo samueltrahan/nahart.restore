fe25637c74b1ab4edf12ff2aec7da82312c2d252
Samuel Trahan <samtrahan91@gmail.com>
2021-06-06 17:02:23 -0500

    footer section added and somewhat complete

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,8 +3,15 @@ import './footer.css';
 
 export default function Footer() {
     return (
-        <div>
-            Footer Section
+        <div className="footer-section">
+            <div className="copy-right">
+                &copy; 2021 by &nbsp; <a className="footer-link" href="https://www.samueltrahan.com">Nahart Web Design</a>
+            </div>
+            <div className="social-links">
+                <a className="footer-link" href="/"><i className="fab fa-instagram fa-2x"></i></a>&nbsp;
+                <a className="footer-link" href="/"><i className="fab fa-twitter fa-2x"></i></a>&nbsp;
+                <a className="footer-link" href='/'><i className="fab fa-amazon fa-2x"></i></a>&nbsp;
+            </div>
         </div>
     )
 }
