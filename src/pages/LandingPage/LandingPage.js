db1d97ecd57d25242d1ffd9410cb27e88de8383b
Samuel Trahan <samtrahan91@gmail.com>
2021-06-15 19:09:25 -0500

    scotty cameron picture added

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './LandingPage.css';
+import ScottyCameron from '../../assets/scotty.jpeg';
 
 export default function LandingPage() {
     return (
-        <div>
-            Landing Page
+        <div className="landing-page">
+            <div className="high-region">
+                <img className="scotty" alt="" src={ScottyCameron}></img>
+            </div>
+            <div className="finish-selection">
+                
+            </div>
         </div>
     )
 }
