f87e60801ba5dda255f9f49d8c1154ad54af290e
Samuel Trahan <samtrahan91@gmail.com>
2021-11-26 20:57:50 -0600

    styled contact page

--- src/pages/ContactPage/Contact.js
+++ src/pages/ContactPage/Contact.js
@@ -11,20 +11,29 @@ export default function Contact() {
                 </div>
                 <div className="email-section">
                     <h3 className="email">Email</h3>
+                    <input className="email-input"></input>
+                    <h3 className="email-msg">Message</h3>
                     <textarea className="text-area"></textarea>
-                    <button>Send</button>
+                    <button className="contact-btn">Send</button>
                 </div>
                 <div className="vendor-secton">
                     <hr></hr>
                     <p className="vendor">Companies</p>
+                    <div className="company-section">
+                    <div className="first-companies">
                     <a className="vendor-link" href="https://www.scottycameron.com" target="_blanket">Scotty Cameron</a><br/>
                     <a className="vendor-link" href="https://www.odysseygolf.com" target="_blanket">Odyssey</a><br/>
                     <a className="vendor-link" href="https://www.ping.com" target="_blanket">Ping</a><br/>
                     <a className="vendor-link" href="https://www.titleist.com" target="_blanket">Titleist</a><br />
+                    </div>
+                    <div className="second-companies">
                     <a className="vendor-link" href="https://www.taylormadegolf.com" target="_blanket">TaylorMade</a><br/>
                     <a className="vendor-link" href="https://www.callaway.com" target="_blanket">Callaway</a><br/>
                     <a className="vendor-link" href="https://www.mizunogolf.com" target="_blanket">Mizuno</a><br/>
                     <a className="vendor-link" href="https://www.clevelandgolf.com" target="_blanket">Cleveland</a><br/>
+                    </div>
+
+                    </div>
                 </div>
             </div>
     )
