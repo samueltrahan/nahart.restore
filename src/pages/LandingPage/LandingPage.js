0aeaa98f6446adefaed9a64e950597ca46766eeb
Samuel Trahan <samtrahan91@gmail.com>
2022-01-15 21:44:49 -0600

    trying to set default

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./LandingPage.css";
 import ScottyCameron from '../../assets/newport-putter.jpeg';
 import FinishCard from '../../components/FinishCard/FinishCard';
@@ -8,6 +8,29 @@ import RestoreFinish from '../../components/RestoreFinish/RestoreFinish';
 import CopperFinish from '../../components/CopperFinish/CopperFinish';
 
 export default function LandingPage() {
+
+
+
+ /* if (typeof window !== "undefined") {
+    return { width: 375, height: 812 };
+  }
+  const isSSR = typeof window != 'undefined';
+  const [windowSize, setWindowSize] = useState({
+    width: isSSR ? 375 : window.innerWidth,
+    height: isSSR ? 812 : window.innerHeight,
+  });
+
+  function changeWindowSize() {
+    setWindowSize({ width: window.innerWidth, height: window.innerHeight });
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", changeWindowSize);
+
+    return () => {
+      window.removeEventListener("resize", changeWindowSize);
+    };
+  }, []);*/
 
   return (
     <div className="landing-page">
